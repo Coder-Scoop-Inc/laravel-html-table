bf32db0fd1019627620ad553ebdbe0d04ec48085
oscarj <oscarj@localhost.localdomain>
2017-06-10 13:48:38 -0400

    html table template

--- src/Views/template.blade.php
+++ src/Views/template.blade.php
@@ -1,2 +1,76 @@
-<h1>Hello world!</h1>
+<!DOCTYPE html>
+<html lang="en">
+	<!-- Head -->
+	<head>
+		<!-- Meta Data
+        ============================================================== -->
+    <title>CoderScoop ContactForm / HTML template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keyWords" content="laravel contact form">
+		<meta charset="utf-8">
+
+		<!-- Fonts, CSS & JS
+        ============================================================== -->
+		<link rel="icon" href="../assets/img/icon.png">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Taviraj" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:500" >
+		<link rel="stylesheet" href="../assets/css/normalize.css">
+		<link rel="stylesheet" href="../assets/css/app.css">
+	</head>
+
+	<!-- Body -->
+	<body>
+		<!-- html table
+				============================================================== -->
+        <table class="laravel.table_responsive">
+			<thead>
+      <tr>
+        <th>#</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+        <th>Table heading</th>
+      </tr>
+    </thead>
+			<tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+        <td>Table cell</td>
+      </tr>
+    </tbody>
+  </table>
+
+
+		<!-- Footer
+				============================================================== -->
+		<footer>
+		</footer>
+	</body>
+</html>
+
 
